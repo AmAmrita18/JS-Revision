4c836594ce99326ed1069b6de96b096e3eb2917d
Amrita Gupta <amamrita0818@gmail.com>
2024-06-20 18:21:29 +0530

    variables and datatypes

---
+++ 1-basics/1_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12232;
+let accountHolder = "Amrita Gupta";
+var emailId = "amrita@gmail.com"
+
+// accountId = 32232; //assignment to constant variable is not allowed
+accountHolder = "Daisy"
+emailId="daisy@gmail.com"
+
+console.table([accountId, accountHolder, emailId])
+
+
+//notes
+// prefer not to use var
+// because of issues in block scope and functional scope
---
+++ 1-basics/2_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we r using nodeJS not browser
+
+//number => 2 to power 53
+//bigint
+//string => "
+//boolean => true/false
+//null => standalone value
+//undefiined
+//symbol => unique
+
+
+//object
+
+console.log(typeof "hitesh")  //string
+console.log(typeof undefined)  //undefined
+console.log(typeof null)  //object
